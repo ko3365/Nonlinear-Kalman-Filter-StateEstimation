82eed21521d275b7a2460926eb6338477ebe3c4d
ko3365 <ko3365@g.ucla.edu>
2022-04-27 20:17:18 -0700

    added central difference KF method

--- nonlinear_KF.m
+++ nonlinear_KF.m
@@ -15,6 +15,11 @@ xstore(1,1) = x; % initial value
 xhatstore = zeros(1,iteration);
 SigXstore = zeros(1^2,iteration);
 
+% Input, Output store for comparison: 
+ustore = zeros(1,iteration+1);
+ustore(1,1) = u;
+zstore = zeros(1,iteration);
+
 %nonlinear System  x = (4+x)^(1/3)+w+2*u,  x^3+3*v
 
 %% Extended KF
@@ -43,14 +48,67 @@ for k=1:iteration
     xhat = xhat+L*(z-zhat);
     xhat = max(-4,xhat);
     SigX = SigX-L*Cty*SigX;
-
+    
+    % Store Data for Plotting
     xstore(1,k+1) = x(:);
     xhatstore(1,k) = xhat;
     SigXstore(1,k) = SigX(:);
+    
+    % Later Use for Comparison
+    ustore(1,k+1)=u;
+    zstore(1,k) = z;
 end
 
+%% Central Difference Kalman Filter
+Nxa = 3;
+h = sqrt(3);
+alpham(1) = (h*h-Nxa)/(h*h);
+alpham(2) = 1/(2*h*h);
+alphamk = alpham(2);
+alphac = alpham;
+alpha = [alpham(1) alphamk(:,ones([1 2*Nxa]))]';
 
+
+xhat2 = 2;
+SigX2 = 1;
+
+xhatstore2 = zeros(iteration,length(xhat2));
+SigXstore2 = zeros(iteration,length(xhat2)^2);
+
+for k=1:iteration
+    
+    % Prediction - Obtain Sigma Points
+    xhat_aug = [xhat2;0;0];
+    Sig_aug = blkdiag(SigX2,Sig_w,Sig_v);
+    sqr_Sig_aug=chol(Sig_aug,'lower');
+    X = xhat_aug(:,ones([1 2*Nxa+1]))+h*[zeros(Nxa,1), sqr_Sig_aug,-sqr_Sig_aug];
+    % Prediction - xhat, zhat
+    X_x = (4+X(1,:)).^(1/3)+X(2,:)+2*ustore(1,k);
+    xhat2 = X_x*alpha;
+    square_X = (X_x(:,2:end) - xhat2(:,ones(1,2*Nxa)))*sqrt(alphac(2));
+    square_X1 =X_x(:,1)-xhat2;
+    SigX2 = square_X*square_X'+alphac(1)*square_X1*square_X1;
+    Z = X_x.^3+3*X(3,:);
+    zhat2 = Z*alpha;
+
+    % Correction
+    square_Z = (Z(:,2:end)-zhat2*ones(1,2*Nxa))*sqrt(alphac(2));
+    square_Z1 = Z(:,1)-zhat2;
+    SigZ = square_Z*square_Z'+alphac(1)*square_Z1*square_Z1';
+    SigXZ = square_X*square_Z'+alphac(1)*square_X1*square_Z1';
+    Lsp = SigXZ/SigZ;
+
+    xhat2 = xhat2+Lsp*(zstore(1,k)-zhat2);
+    SigX2 = SigX2-Lsp*SigZ*Lsp';
+
+    xhatstore2(k,:) = xhat2;
+    SigXstore2(k,:) = SigX2(:);
+end
+
+%% Plot
 figure(1)
+
+subplot(2,1,1)
 hold on
 plot(0:iteration-1,xstore(1:iteration)','k-')
 plot(0:iteration-1,xhatstore','r-')
@@ -59,3 +117,17 @@ plot(0:iteration-1,xhatstore'+3*sqrt(SigXstore'), 'b--', ...
 grid on
 legend('true x','estimate x (Extended KF)','error bounds')
 title("Extended Kalman Filter")
+ylim([-2,13])
+hold off
+
+subplot(2,1,2)
+hold on
+plot(0:iteration-1,xstore(1:iteration)','k-')
+plot(0:iteration-1,xhatstore2','r-')
+plot(0:iteration-1,xhatstore2'+3*sqrt(SigXstore2'), 'b--', ...
+    0:iteration-1,xhatstore2'-3*sqrt(SigXstore2'), 'b--')
+grid on
+legend('true x','estimate x (CDKF)','error bounds')
+ylim([-2,13])
+title("Central Difference Kalman Filter")
+hold off
